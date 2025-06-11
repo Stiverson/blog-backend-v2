e77d8665969209abefd7cbbc4d70188661cc9641
root <root@DESKTOP-MC83SFJ>
2025-06-10 23:52:17 -0300

    feat: configure express server and database connection

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+// Middlewares
+app.use(morgan('dev'));   
+app.use(cors());           
+app.use(express.json());   
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API Blog Backend v2.0 funcionando!');
+});
+
+module.exports = app;

ea8c982eb7e3c02265a75c10b0a5c66255636601
root <root@DESKTOP-MC83SFJ>
2025-10-28 14:11:48 -0300

    final: ajustes tipo  de comunicado para o mobile

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -8,7 +8,8 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const postValidationRules = [
   body('title').notEmpty().withMessage('O título é obrigatório'),
   body('content').notEmpty().withMessage('O conteúdo é obrigatório'),
-  body('author').notEmpty().withMessage('O autor é obrigatório')
+  body('author').notEmpty().withMessage('O autor é obrigatório'),
+  body('tipo').isIn(['Comunicado', 'Aviso', 'Outros']).withMessage('O tipo de comunicado é obrigatório e deve ser válido.')
 ];
 
 
@@ -45,7 +46,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', authMiddleware.protect, postValidationRules, validate, async (req, res) => {
   try {
     const { title, content, author } = req.body;
-    const newPost = new Post({ title, content, author });
+    const newPost = new Post({ title, content, author, tipo });
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
   } catch (error) {
@@ -56,10 +57,10 @@ router.post('/', authMiddleware.protect, postValidationRules, validate, async (r
 
 router.put('/:id', authMiddleware.protect, postValidationRules, validate, async (req, res) => {
   try {
-    const { title, content, author } = req.body;
+    const { title, content, author, tipo } = req.body;
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.id,
-      { title, content, author, updatedAt: new Date() },
+      { title, content, author,tipo, updatedAt: new Date() },
       { new: true }
     );
     if (!updatedPost) return res.status(404).json({ message: 'Post não encontrado' });
